8ac547e1475139535f3f2bf46f5f0431db969826
Vincent Déniel <denielvincent@gmail.com>
2017-05-28 00:35:01 +0200

    fixed empty images

--- imgur_api/__init__.py
+++ imgur_api/__init__.py
@@ -3,6 +3,14 @@ async def extract_elements_from_response(response_json):
     if not data or len(data) == 0:
         return
     extracted_elements = [
-        (entry.get("title"), f"https://i.imgur.com/{entry.get('cover')}.jpg", entry.get("link")) for entry in data
+        (entry.get("title"), await get_cover(entry), entry.get("link")) for entry in data
     ]
     return extracted_elements
+
+
+async def get_cover(entry):
+    cover = entry.get('cover')
+    cover_link = f"https://i.imgur.com/{cover}b.jpg"
+    if not cover:
+        cover_link = entry.get("link")
+    return cover_link
